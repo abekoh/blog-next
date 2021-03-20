36f9fa96ddec6f28684243e8863e47f0fad9a46b
abekoh <abekoh.bcky@gmail.com>
2021-03-20 16:42:50 +0900

    add api.ts

---
+++ src/types/api.ts
@@ -0,0 +1,31 @@
+export type ListContentsResponse<T> = {
+  contents: T[];
+  totalCount: number;
+  offset: number;
+  limit: number;
+};
+
+export type ContentResponse<T> = {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+};
+
+export type GetListContentsQuery = {
+  draftKey?: string;
+  limit?: number;
+  orders?: string;
+  q?: string;
+  fields?: string;
+  ids?: string;
+  filters?: string;
+  depth?: number;
+};
+
+export type GetContentsQuery = {
+  draftKey?: string;
+  fields?: string;
+  depth?: number;
+};
