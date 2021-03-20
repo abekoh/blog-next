376c040c0b3e79773d3d4dfc1f9199dbabdde49c
abekoh <abekoh.bcky@gmail.com>
2021-03-20 18:14:16 +0900

    add blog page

---
+++ src/pages/posts/[id]/index.tsx
@@ -0,0 +1,73 @@
+import {
+  GetStaticPaths,
+  GetStaticProps,
+  InferGetServerSidePropsType,
+  NextPage,
+} from "next";
+import { useRouter } from "next/dist/client/router";
+import Link from "next/link";
+
+import { PostResponse } from "../../../types/post";
+import { client } from "../../../utils/api";
+import { toStringId } from "../../../utils/toStringId";
+
+type StaticProps = {
+  post: PostResponse;
+  draftKey?: string;
+};
+type PageProps = InferGetServerSidePropsType<typeof getStaticProps>;
+
+const Page: NextPage<PageProps> = (props) => {
+  const { post } = props;
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <>
+      <nav>
+        <Link href="/">Home</Link>
+      </nav>
+      <main>
+        <header>
+          <h1>{post.title}</h1>
+        </header>
+        {post.body && (
+          <article dangerouslySetInnerHTML={{ __html: post.body }} />
+        )}
+      </main>
+    </>
+  );
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    fallback: "blocking",
+    paths: [],
+  };
+};
+
+export const getStaticProps: GetStaticProps<StaticProps> = async (context) => {
+  const { params } = context;
+  if (!params?.id) {
+    throw new Error("Error: not found");
+  }
+  const id = toStringId(params.id);
+  try {
+    const post = await client.v1.posts._id(id).$get({
+      query: {
+        fields: "id,title,body",
+      },
+    });
+    return {
+      props: { post },
+      revalidate: 60,
+    };
+  } catch (e) {
+    return { notFound: true };
+  }
+};
+
+export default Page;
