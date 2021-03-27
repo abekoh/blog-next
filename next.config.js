4c7546ea9dec6f1f06437ef9edab0e4bbf932c27
Kotaro Abe <abekoh.bcky@gmail.com>
2021-03-27 18:52:11 +0900

    add redirect config

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    async redirects() {
+        return [
+            {
+                source: '/post/:id',
+                destination: '/posts/:id',
+                permanent: true,
+            },
+        ]
+    },
+}
