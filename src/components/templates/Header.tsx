49b9565b3ffb581f61b37b9eac9a38060faad406
abekoh <abekoh.bcky@gmail.com>
2021-04-02 01:05:22 +0900

    update

--- src/components/templates/Header.tsx
+++ src/components/templates/Header.tsx
@@ -1,35 +1,49 @@
 import React from 'react';
 
-import { AppBar } from '@material-ui/core';
+import { AppBar, makeStyles, MenuItem, MenuList } from '@material-ui/core';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Link from 'next/link';
+
+const useStyles = makeStyles((theme) => ({
+  titleLink: {
+    textDecoration: 'none',
+  },
+  title: {
+    color: theme.palette.text.primary,
+  },
+  menuList: {
+    display: 'flex',
+  },
+}));
 
 type Props = {
   blogTitle: string;
 };
 const Header: React.FC<Props> = ({ blogTitle }) => {
+  const classes = useStyles();
   return (
     <header>
       <AppBar position="static">
         <Toolbar>
-          <Typography component="h1" variant="h5">
-            {blogTitle}
-          </Typography>
-          <ul>
-            <li>
-              <Link href={`/`}>Home</Link>
-            </li>
-            <li>
-              <Link href={`/about`}>About</Link>
-            </li>
-            <li>
-              <Link href={`/posts`}>Posts</Link>
-            </li>
-            <li>
-              <Link href={`/tags`}>Tags</Link>
-            </li>
-          </ul>
+          <Link href={`/`}>
+            <a className={classes.titleLink}>
+              <Typography component="h1" variant="h5" className={classes.title}>
+                {blogTitle}
+              </Typography>
+            </a>
+          </Link>
+          <MenuList className={classes.menuList}>
+            <Link href={`/about`}>
+              <MenuItem>About</MenuItem>
+            </Link>
+            <Link href={`/posts`}>
+              <MenuItem>Posts</MenuItem>
+            </Link>
+            <Link href={`/tags`}>
+              <MenuItem>Tags</MenuItem>
+            </Link>
+          </MenuList>
         </Toolbar>
       </AppBar>
     </header>
