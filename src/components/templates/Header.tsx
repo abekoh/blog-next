5c8368ec1b53836552f586954680dbdb793f64ac
abekoh <abekoh.bcky@gmail.com>
2021-04-02 00:03:25 +0900

    update

--- src/components/templates/Header.tsx
+++ src/components/templates/Header.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import { AppBar } from '@material-ui/core';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import Link from 'next/link';
 
 type Props = {
@@ -8,21 +11,27 @@ type Props = {
 const Header: React.FC<Props> = ({ blogTitle }) => {
   return (
     <header>
-      <h1>{blogTitle}</h1>
-      <ul>
-        <li>
-          <Link href={`/`}>Home</Link>
-        </li>
-        <li>
-          <Link href={`/about`}>About</Link>
-        </li>
-        <li>
-          <Link href={`/posts`}>Posts</Link>
-        </li>
-        <li>
-          <Link href={`/tags`}>Tags</Link>
-        </li>
-      </ul>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography component="h1" variant="h5">
+            {blogTitle}
+          </Typography>
+          <ul>
+            <li>
+              <Link href={`/`}>Home</Link>
+            </li>
+            <li>
+              <Link href={`/about`}>About</Link>
+            </li>
+            <li>
+              <Link href={`/posts`}>Posts</Link>
+            </li>
+            <li>
+              <Link href={`/tags`}>Tags</Link>
+            </li>
+          </ul>
+        </Toolbar>
+      </AppBar>
     </header>
   );
 };
