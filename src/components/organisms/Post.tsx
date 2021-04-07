cff3f050409fe33a69ce5f627aa00dd01c95c93b
abekoh <abekoh.bcky@gmail.com>
2021-04-07 22:41:37 +0900

    update

--- src/components/organisms/Post.tsx
+++ src/components/organisms/Post.tsx
@@ -1,17 +1,29 @@
 import React from 'react';
 
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  article: {
+    '& p': {
+      color: theme.palette.text.primary,
+    },
+  },
+}));
+
 type Props = {
   title: string;
   body: string;
 };
 
 const Post: React.FC<Props> = ({ title, body }) => {
+  const classes = useStyles();
   return (
     <>
       <header>
         <h2>{title}</h2>
       </header>
       <article
+        className={classes.article}
         dangerouslySetInnerHTML={{
           __html: body,
         }}
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -2,7 +2,7 @@ import { createMuiTheme } from '@material-ui/core';
 
 const theme = createMuiTheme({
   palette: {
-    mode: 'dark',
+    mode: 'light',
   },
 });
 
