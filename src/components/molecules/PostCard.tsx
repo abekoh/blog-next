5d0ff48528a6eea0cc71463a87bf3a4a707d19cb
Kotaro Abe <abekoh.bcky@gmail.com>
2021-04-11 15:10:57 +0900

    update PostCard

--- src/components/molecules/PostCard.tsx
+++ src/components/molecules/PostCard.tsx
@@ -1,4 +1,5 @@
 import { Card, CardActionArea, CardContent, makeStyles, Typography } from "@material-ui/core";
+import { grey } from "@material-ui/core/colors";
 import { TagResponse } from "../../types/tag";
 import { dateToString } from "../../utils/dateToString";
 import Link from "../utils/Link";
@@ -6,7 +7,15 @@ import TagList from "./TagList";
 
 const useStyles = makeStyles(() => ({
     title: {
-    }
+    },
+    link: {
+        '&:hover': {
+            textDecoration: 'none',
+        },
+    },
+    dateText: {
+        color: grey[500],
+    },
 }));
 
 type Props = {
@@ -19,12 +28,12 @@ type Props = {
 const PostCard: React.FC<Props> = ({ id, title, publishedAt, tags }) => {
     const classes = useStyles();
     return (
-        <Link href={`/posts/${id}`}>
+        <Link href={`/posts/${id}`} className={classes.link}>
             <Card sx={{ display: 'flex' }}>
                 <CardActionArea>
                     <CardContent>
                         <Typography variant="h5">{title}</Typography>
-                        <Typography variant="subtitle1">{dateToString(publishedAt)}</Typography>
+                        <Typography variant="subtitle1" className={classes.dateText}>{dateToString(publishedAt)}</Typography>
                         <TagList tags={tags} linkable={false} />
                     </CardContent>
                 </CardActionArea>
