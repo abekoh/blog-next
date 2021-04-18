655cb7f7e553ce3e34bccf1bc9fd524dcd72244e
abekoh <abekoh.bcky@gmail.com>
2021-04-19 00:19:59 +0900

    fix url

--- now-build.sh
+++ now-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 yum install wget
-wget https://github.com/jwerre/node-canvas-lambda/raw/master/node14_canvas_lib64_layer.zip
+wget https://github.com/jwerre/node-canvas-lambda/raw/master/node12_canvas_lib64_layer.zip
 unzip -j -d canvas_lib64 node12_canvas_lib64_layer.zip
 cp canvas_lib64/*so.1 node_modules/canvas/build/Release/
 yarn build
