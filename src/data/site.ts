10231b0e36d888b986caffbedfca2a51b8649c1e
abekoh <abekoh.bcky@gmail.com>
2021-04-18 23:53:38 +0900

    update twitter ogp config

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,6 +1,7 @@
 export const siteData = {
   title: "abekoh's tech note",
   author: 'abekoh',
+  twitterUserName: 'abekoh_bcky',
   copyright: '© 2019-2021 abekoh',
   copyrightStartYear: 2019,
   hostname: 'blog.abekoh.dev',
