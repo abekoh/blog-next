a808251dd4493ff5ca54da62f4d73cdd77ab213f
abekoh <abekoh.bcky@gmail.com>
2021-05-08 22:43:01 +0900

    fix top

--- src/components/organisms/TopProfile.tsx
+++ src/components/organisms/TopProfile.tsx
@@ -11,6 +11,7 @@ import {
   Button,
   CardActions,
   Box,
+  Avatar,
 } from '@material-ui/core';
 
 import Link from '../utils/Link';
@@ -26,30 +27,48 @@ type Props = {
 const TopProfile: React.FC<Props> = ({ title, body, children }) => {
   const classes = useStyles();
   return (
-    <Card sx={{ display: 'flex' }}>
-      <CardMedia
-        component="img"
-        sx={{ width: 64, margin: '1.0rem' }}
-        image="/logo.png"
-        title="hoge"
-      />
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-        }}
-      >
-        <CardContent>
-          <Typography>
-            abekohが雑多に技術メモを書いていくブログです。たまに技術以外のことも書くかも。
-          </Typography>
-        </CardContent>
-        <CardActions sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-          <Link href="/about">
-            <Button size="small">{'>>'} More</Button>
-          </Link>
-        </CardActions>
-      </Box>
+    <Card>
+      <Grid container>
+        <Grid
+          item
+          xs={12}
+          md={2}
+          sx={{ display: 'flex', justifyContent: 'center' }}
+        >
+          <Box sx={{ padding: '1.0rem' }}>
+            <Avatar
+              src="/logo.png"
+              title="abekoh"
+              variant="square"
+              sx={{ width: 64, height: 64 }}
+            />
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={10}>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+            }}
+          >
+            <CardContent>
+              <Typography>
+                abekohが雑多に技術メモを書いていくブログです。たまに技術以外のことも書くかも。
+              </Typography>
+            </CardContent>
+            <CardActions
+              sx={{
+                display: 'flex',
+                justifyContent: 'flex-end',
+              }}
+            >
+              <Link href="/about">
+                <Button size="small">{'>>'} More</Button>
+              </Link>
+            </CardActions>
+          </Box>
+        </Grid>
+      </Grid>
     </Card>
   );
 };
