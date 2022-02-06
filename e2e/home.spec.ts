6df1256dfed2a59484029326933840770d63230b
Kotaro Abe <abekoh.bcky@gmail.com>
2022-02-06 14:43:11 +0900

    update test

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -4,4 +4,9 @@ test('home is shown correctly', async ({ page }) => {
   await page.goto('https://blog.abekoh.dev/');
   await expect(page).toHaveTitle(/abekoh's tech note/);
   await expect(page.locator('h1')).toHaveText(/abekoh's tech note/);
+
+  await expect(page.locator('text=Recent Posts').first()).toBeVisible();
+  await expect(
+    page.locator('a:has-text("Privacy Policy")').first(),
+  ).toHaveAttribute('href', '/privacy');
 });
