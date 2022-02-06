bb6b24ff2f0854f49cc40984578e9d0b559049f3
Kotaro Abe <abekoh.bcky@gmail.com>
2022-02-06 16:41:26 +0900

    fix (#61)
    * fix
    
    * fix
    
    * update test
    
    * rename
    
    * update snapshot

--- e2e/e2e.spec.ts
+++ e2e/e2e.spec.ts
@@ -11,10 +11,16 @@ test.describe('home', () => {
     await page.goto(BASE_URL);
     await expect(page).toHaveTitle(/abekoh's tech note/);
     await expect(page.locator('h1')).toHaveText(/abekoh's tech note/);
-
     await expect(page.locator('text=Recent Posts').first()).toBeVisible();
     await expect(
       page.locator('a:has-text("Privacy Policy")').first(),
     ).toHaveAttribute('href', '/privacy');
+  });
+});
+
+test.describe('ogp', () => {
+  test('home is shown correctly', async ({ page }) => {
+    await page.goto(`${BASE_URL}/api/ogp-images/start-blog`);
+    expect(await page.screenshot()).toMatchSnapshot('data/start-blog.png');
   });
 });
