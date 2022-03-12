e19ec54f53cdbd6a48c798ff8df328bca33a03d3
Kotaro Abe <abekoh.bcky@gmail.com>
2022-03-12 14:42:19 +0900

    set alt

--- src/components/atoms/MyAvatar.tsx
+++ src/components/atoms/MyAvatar.tsx
@@ -3,9 +3,10 @@ import { Avatar } from '@mui/material';
 const MyAvatar: React.FC = () => {
   return (
     <Avatar
-      src="/logo.png"
-      title="abekoh"
-      variant="square"
+      src='/logo.png'
+      title='abekoh'
+      alt="abekoh's icon"
+      variant='square'
       sx={{ width: 64, height: 64 }}
     />
   );
